52df882f2cd8a673baf84d23292bc14ab648d6e8
o9 <shno59.9009@gmail.com>
2026-07-14 09:03:04 +0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,30 +3,36 @@ if (-Not ($Env:WT_SESSION)) {
     return
 }
 
+
 if ($PSVersionTable.PSVersion.Major -ne 7) {
     Write-Host "Use PowerShell 7 to Install" -ForegroundColor Red
     return
 }
+
 
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Host "Run Script as Administrator" -ForegroundColor Red
     return
 }
 
+
 if (-Not (Test-Connection 8.8.8.8 -Count 1 -TimeoutSeconds 1 -Quiet)) { 
     Write-Host "Activate internet connection" -ForegroundColor Red
     return
 }
 
+
 if (Test-Path $Profile) {
-    Move-Item -Path $Profile -Destination ($Profile + ".bak")
+    Move-Item $Profile "$Profile.bak" -Force
 } else {
-    New-Item -Path $Profile -Force | Out-Null
+    New-Item -ItemType File -Path $Profile -Force | Out-Null
 }
+
 
 Write-Host "Profile..." -ForegroundColor Cyan
 Invoke-WebRequest -Uri https://github.com/o9-9/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $Profile
 Write-Host "✔ Profile" -ForegroundColor Green
+
 
 Write-Host "Theme..." -ForegroundColor Cyan
 $themeDir = "$env:USERPROFILE\.config\ohmyposh"
@@ -37,27 +43,34 @@ if (-not (Test-Path $themeFile)) {
 }
 Write-Host "✔ Theme" -ForegroundColor Green
 
+
 Write-Host "Fonts..." -ForegroundColor Cyan
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/o9-9/powershell-profile/main/stuff/fonts.zip" -OutFile "$env:TEMP\fonts.zip"
-Expand-Archive -Path fonts.zip
-Get-ChildItem fonts -Filter *.ttf | ForEach-Object {
+$zip = Join-Path $env:TEMP "fonts.zip"
+$dir = Join-Path $env:TEMP "fonts"
+Invoke-WebRequest "https://raw.githubusercontent.com/o9-9/powershell-profile/main/stuff/fonts.zip" -OutFile $zip
+Remove-Item $dir -Recurse -Force -ErrorAction Ignore
+Expand-Archive $zip -DestinationPath $dir -Force
+Get-ChildItem $dir -Recurse -Filter *.ttf | ForEach-Object {
     ((New-Object -ComObject Shell.Application).Namespace(0x14)).CopyHere($_.FullName)
-    Write-Host -ForegroundColor Green "✔ Font $(Split-Path $_ -Leaf)"
+    Write-Host "✔ Font $($_.Name)" -ForegroundColor Green
 }
-Remove-Item -Path fonts.zip
-Remove-Item -Path fonts -Recurse
+Remove-Item $zip,$dir -Recurse -Force
+
 
 Write-Host "Dependencies..." -ForegroundColor Cyan
 Install-PackageProvider -Name NuGet -Force -ErrorAction SilentlyContinue | Out-Null
 Install-Module -Name Terminal-Icons -Force
 Write-Host "✔ Dependencies" -ForegroundColor Green
 
+
 Write-Host "Oh My Posh..." -ForegroundColor Cyan
 winget install JanDeDobbeleer.OhMyPosh --source winget --silent
 Write-Host "✔ Oh My Posh" -ForegroundColor Green
 
+
 Write-Host "Zoxide..." -ForegroundColor Cyan
 winget install ajeetdsouza.zoxide --source winget --silent
 Write-Host "✔ Zoxide" -ForegroundColor Green
+
 
 Write-Host "✔ Complete" -ForegroundColor Green
